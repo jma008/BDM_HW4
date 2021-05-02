bb53097034313d957e9deffa14573c2391ec30fa
Juliana <70915646+jma008@users.noreply.github.com>
2021-05-02 00:23:10 -0400

    Update BDM_HW4_Ma.py

--- BDM_HW4_Ma.py
+++ BDM_HW4_Ma.py
@@ -44,7 +44,7 @@ def date_conversion(x):
     start_date = datetime.datetime.strptime(x[0], "%Y-%m-%d")
     # end_date = datetime.datetime.strptime(x[1][1][:10], "%Y-%m-%d")
     visits_by_day = json.loads(x[1])
-    return [((start_date + datetime.timedelta(days=day)).date().isoformat(), [int(visit)])
+    return [((start_date + datetime.timedelta(days=day)).date(), [int(visit)])
             for day, visit in enumerate(visits_by_day)]
 
 
@@ -77,7 +77,7 @@ def main(sc):
             .mapPartitionsWithIndex(extract_visits) \
             .filter(lambda x: x[0] in place_id) \
             .map(lambda x: (x[1][0][:10], x[1][1])) \
-            .filter(lambda x: x[0][:4] in ['2019', '2020']) \
+            .filter(lambda x: x[0][:4] != '2018') \
             .flatMap(date_conversion) \
             .reduceByKey(lambda x, y: x + y) \
             .map(computations) \
